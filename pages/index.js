962baf8c7a9d1df0e324f62fe8a1b4d6327e9c81
Daniel Malekzadeh <daniel.malekzadeh@hotmail.com>
2022-12-26 20:48:50 +0330

    create database schema and create home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import React from "react";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world.</h1>
-    </div>
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["procut 1", "product 2"].map((product, idx) => product)}
+      </div>
+      Footer
+    </>
   );
 };
 
