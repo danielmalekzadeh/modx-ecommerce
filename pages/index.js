8884538d8022431385fc14e9ef6199b2b344f459
Daniel Malekzadeh <daniel.malekzadeh@hotmail.com>
2022-12-31 17:02:04 +0330

    complete the cart box and etc...

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { client } from "../lib/client";
 
-const Home = () => {
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData?.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["procut 1", "product 2"].map((product, idx) => product)}
+        {products?.map((product, idx) => (
+          <Product product={product} key={idx} />
+        ))}
       </div>
-      Footer
+      <FooterBanner FooterBanner={bannerData && bannerData[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
